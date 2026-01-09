792b1e554cff3dacb41985a4079acf3cb4d0b16b
Hanzalaq24 <24se09ml012@ppsu.ac.in>
2026-01-10 01:00:39 +0530

    Update return policy text to 7 Days

--- src/app/product/[id]/ProductDetailClient.tsx
+++ src/app/product/[id]/ProductDetailClient.tsx
@@ -344,7 +344,7 @@ const ProductDetailClient: React.FC<ProductDetailClientProps> = ({ product, simi
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4" />
                                     </svg>
-                                    <span className="text-sm text-gray-300">10 Days Easy Return</span>
+                                    <span className="text-sm text-gray-300">7 Days Easy Return*</span>
                                 </div>
                                 <div className="flex items-center">
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
